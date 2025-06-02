ebad949114c7e81c4102fca717da3dfa044caf91
Wladyslaw Pastushok <uladzislau.pastushok@student.put.poznan.pl>
2025-06-02 23:33:07 +0200

    WORKING 4.0!

--- mpi/main.cpp
+++ mpi/main.cpp
@@ -8,12 +8,12 @@
 #include <unistd.h>
 
 #define REQUEST 0
-#define ACK 1
 #define RELEASE 2
+#define GRANT 3
+#define DONE 4
 
 int rank, size;
 int lamport_clock = 0;
-int ack_count = 0;
 
 #define GATE_CAPACITY 2 // Default Y=2
 typedef enum { NONE = 0, DIR_LEFT = 1, DIR_RIGHT = 2 } Direction;
@@ -77,16 +77,11 @@ int compare_requests(Request a, Request b) {
   return a.rank - b.rank;
 }
 
+#include <algorithm>
 void sort_queue() {
-  for (int i = 0; i < queue_size - 1; i++) {
-    for (int j = i + 1; j < queue_size; j++) {
-      if (compare_requests(request_queue[i], request_queue[j]) > 0) {
-        Request temp = request_queue[i];
-        request_queue[i] = request_queue[j];
-        request_queue[j] = temp;
-      }
-    }
-  }
+  std::ranges::sort(request_queue, request_queue + queue_size, [](const Request& a, const Request& b) {
+    return compare_requests(a, b) < 0;
+  });
 }
 
 void add_request(Request req) {
@@ -95,42 +90,37 @@ void add_request(Request req) {
 }
 
 void remove_request(int from_rank) {
-  int i;
-  for (i = 0; i < queue_size; i++) {
-    if (request_queue[i].rank == from_rank)
-      break;
+  auto it = std::ranges::find_if(request_queue, request_queue + queue_size, [from_rank](const Request& req) {
+    return req.rank == from_rank;
+  });
+  if (it != request_queue + queue_size) {
+    std::ranges::move(it + 1, request_queue + queue_size, it);
+    --queue_size;
   }
-  for (; i < queue_size - 1; i++) {
-    request_queue[i] = request_queue[i + 1];
-  }
-  queue_size--;
 }
 
 int count_top_n_same_direction() {
-  // CURRENTLY IN INITIAL STATE
   if (gate_direction == NONE)
     return 0;
 
-  int count = 0;
-  for (int i = 0; i < queue_size && count < GATE_CAPACITY; i++) {
-    if (request_queue[i].direction == gate_direction)
-      count++;
-    else
-      break;
-  }
-  return count;
+  return std::ranges::count_if(
+    request_queue,
+    request_queue + std::min(queue_size, GATE_CAPACITY),
+    [](const Request& req) {
+      return req.direction == gate_direction;
+    }
+  );
 }
 
 int is_among_top_n(int rank_to_check) {
-  // CURRENTLY IN INITIAL STATE
   if (gate_direction == NONE)
     return 0;
 
   int count = 0;
-  for (int i = 0; i < queue_size; i++) {
+  for (int i = 0; i < std::min(queue_size, GATE_CAPACITY); ++i) {
     if (request_queue[i].direction == gate_direction) {
       if (request_queue[i].rank == rank_to_check)
-        return count < GATE_CAPACITY;
+        return 1;
       count++;
     }
   }
@@ -179,9 +169,6 @@ int can_enter() {
   if (!is_among_top_n(rank))
     return 0;
 
-  if (ack_count < size - 1)
-    return 0;
-
   return 1;
 }
 
@@ -198,12 +185,6 @@ void send_request_to_all() {
   }
 }
 
-void send_ack(int dest) {
-  lamport_clock++;
-  int data[1] = {lamport_clock};
-  MPI_Send(&data, 1, MPI_INT, dest, ACK, MPI_COMM_WORLD);
-}
-
 void send_release_to_all() {
   lamport_clock++;
   for (int i = 0; i < size; i++) {
@@ -214,15 +195,20 @@ void send_release_to_all() {
   }
 }
 
-void enter_critical_section() {
-  auto log = std::format(
-      "GATE DIRECTION: {}, TOP DIRECTION: {}, LAMPORT CLOCK: {}",
-      logger.directionToString(gate_direction),
-      logger.directionToString(request_queue[0].direction), lamport_clock);
+void send_done_to_all() {
+  for (int i = 0; i < size; i++) {
+    int dummy[1] = {0};
+    MPI_Send(&dummy, 1, MPI_INT, i, DONE, MPI_COMM_WORLD);
+  }
+}
+
+void log_enter_leave(Direction gate_dir, Direction top_dir) {
+  auto log =
+      std::format("GATE DIRECTION: {}, TOP DIRECTION: {}, LAMPORT CLOCK: {}",
+                  logger.directionToString(gate_dir),
+                  logger.directionToString(top_dir), lamport_clock);
   logger.log("ENTERING", log);
-
   sleep(2);
-
   logger.log("LEAVING", std::format("Gate at Lamport {}", lamport_clock));
 }
 
@@ -237,44 +223,118 @@ int main(int argc, char **argv) {
   srand(rank + time(NULL));
   sleep(rand() % 3);
 
-  send_request_to_all();
-  add_request(start_state);
-
   MPI_Status status;
 
-  while (1) {
-    int buffer[3];
-    MPI_Recv(buffer, 3, MPI_INT, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD,
-             &status);
+  if (rank == size - 1) { // CORDINATOR
 
-    update_clock(buffer[0]);
+    int in_cs_count = 0;
+    int done_count = 0;
+    int total_p_count = size;
 
-    // RECV
-    if (status.MPI_TAG == REQUEST) {
-      Request req = {buffer[0], buffer[1], static_cast<Direction>(buffer[2])};
-      add_request(req);
-      send_ack(buffer[1]);
-      try_change_direction();
+    while (true) {
+      int buffer[3];
+      MPI_Recv(buffer, 3, MPI_INT, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD,
+               &status);
+      update_clock(buffer[0]);
+      
+      if (status.MPI_TAG == REQUEST) {
+        add_request(
+            Request{buffer[0], buffer[1], static_cast<Direction>(buffer[2])});
+        try_change_direction();
+      } 
+      else if (status.MPI_TAG == RELEASE) { // P LEAVES CS
+        remove_request(status.MPI_SOURCE);
+        in_cs_count--;
+        done_count++;
+        if (in_cs_count == 0) // NO ONE IN CS CAN CHANGE DIR
+          try_change_direction();
+        if (done_count == total_p_count) { // EXIT PROGRAM 
+          send_done_to_all();
+          logger.log("Finaly", "All processes have finished. Exiting.");
+          break;
+        }
 
-    } else if (status.MPI_TAG == ACK) {
-      ack_count++;
-    } else if (status.MPI_TAG == RELEASE) {
-      int releasing_rank = status.MPI_SOURCE;
-      remove_request(releasing_rank);
-      try_change_direction();
+      }
+
+      if (in_cs_count != 0 || queue_size == 0) {
+        if (queue_size == 0)
+          gate_direction = Direction::NONE;
+        continue;
+      }
+      
+      gate_direction = request_queue[0].direction;
+
+      int granted = 0;
+      for (size_t i = 0; i < queue_size && granted < GATE_CAPACITY; ++i) {
+        if (request_queue[i].direction != gate_direction)
+          continue;
+        if (request_queue[i].rank == rank) {
+          log_enter_leave(gate_direction, request_queue[0].direction);
+          remove_request(rank);
+          in_cs_count++;
+          granted++;
+          in_cs_count--;
+          done_count++;
+          if (in_cs_count == 0)
+            try_change_direction();
+          if (done_count == total_p_count) {
+            send_done_to_all();
+            logger.log("Finaly", "All processes have finished. Exiting.");
+            break;
+          }
+          break;
+        } else {
+          int data[3] = {lamport_clock, (int)gate_direction,
+                         (int)request_queue[0].direction};
+          MPI_Send(&data, 3, MPI_INT, request_queue[i].rank, GRANT,
+                   MPI_COMM_WORLD);
+          granted++;
+          in_cs_count++;
+        }
+      }
+      int found = 0;
+      for (int i = 0; i < queue_size; i++) {
+        if (request_queue[i].rank == rank) {
+          found = 1;
+          break;
+        }
+      }
+      if (done_count < total_p_count && !found) {
+        lamport_clock++;
+        add_request(Request{lamport_clock, rank,
+                            (rank % 2 == 0) ? DIR_LEFT : DIR_RIGHT});
+      }
     }
+  } else { // SLAVE ;]
+    lamport_clock++;
+    start_state.timestamp = lamport_clock;
+    start_state.rank = rank;
 
-    // TRY
-    if (can_enter()) {
-      enter_critical_section();
+    // SEND CORDINATOR REQUEST 
+    int data[3] = {start_state.timestamp, rank, (int)start_state.direction};
+    MPI_Send(&data, 3, MPI_INT, size - 1, REQUEST, MPI_COMM_WORLD);
 
-      remove_request(rank);
-      send_release_to_all();
+    while (true) {
+      int buffer[3];
+      MPI_Recv(buffer, 3, MPI_INT, size - 1, MPI_ANY_TAG, MPI_COMM_WORLD,
+               &status);
+      update_clock(buffer[0]);
+      if (status.MPI_TAG == GRANT) {
+        Direction granted_gate_direction = (Direction)buffer[1];
+        Direction granted_top_direction = (Direction)buffer[2];
 
-      break;
+        log_enter_leave(granted_gate_direction, granted_top_direction);
+
+        lamport_clock++;
+        int rel[1] = {lamport_clock};
+        MPI_Send(&rel, 1, MPI_INT, size - 1, RELEASE, MPI_COMM_WORLD);
+        break;
+      }
     }
+    int dummy[1];
+    MPI_Recv(dummy, 1, MPI_INT, size - 1, DONE, MPI_COMM_WORLD, &status);
+    logger.log("Finaly", "All processes have finished. Exiting.");
   }
-
   MPI_Finalize();
   return 0;
 }
